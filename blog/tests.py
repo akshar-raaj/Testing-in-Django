b20e81e4d4c04ddc60d4abc60c537b7182029dfe
Akshar Raaj <akshar@agiliq.com>
2013-04-15 19:05:36 +0530

    Added first test which fails because of absense of view

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,12 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BlogEntriesTest(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_entries_access(self):
+        response = self.c.get('/blog/entries')
+        self.assertEqual(response.status_code, 200)
