9fdb7b1d13061be12bf0f9d8a1939e9d6a385d0e
Akshar Raaj <akshar@agiliq.com>
2013-06-29 21:46:28 +0530

    pep8 fix

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class PublishedBlogManager(models.Manager):
     def get_query_set(self, *args, **kwargs):
-        return super(PublishedBlogManager, self).get_query_set(*args, **kwargs).filter(is_published=True)
+        return super(PublishedBlogManager, self).\
+                get_query_set(*args, **kwargs).filter(is_published=True)
+
 
 class BlogEntry(models.Model):
     title = models.CharField(max_length=100)
